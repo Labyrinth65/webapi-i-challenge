b8496f5ba7e285ccd7cf472a7c1f7af8c8c29a1f
Ang Xu <angxu1234@gmail.com>
2019-07-08 16:53:08 -0500

    get completed

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // implement your API here
+const express = require("express");
+
+const users = require("./data/db.js");
+
+const server = express();
+
+server.use(express.json()); // parse JSON in POST
+
+const port = 5000;
+server.listen(port, () => console.log(`\n*** running on port ${port} ***\n`));
+
+server.get("/api/users", function(req, res) {
+	users
+		.find()
+		.then(users => {
+			res.status(200).json(users);
+		})
+		.catch(error => {
+			res
+				.status(500)
+				.json({ error: "The users information could not be retrieved." });
+		});
+});
+
+server.get("/api/users/:id", function(req, res) {
+	const { id } = req.params;
+	users
+		.findById(id)
+		.then(user => {
+			if (user) {
+				res.status(200).json(user);
+			} else {
+				res
+					.status(404)
+					.json({ message: "The user with the specified ID does not exist." });
+			}
+		})
+		.catch(error => {
+			res
+				.status(500)
+				.json({ error: "The user information could not be retrieved." });
+		});
+});
