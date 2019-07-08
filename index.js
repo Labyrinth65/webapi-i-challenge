fe249666cf9d22064603bd3cba36d8317c597a29
Ang Xu <angxu1234@gmail.com>
2019-07-08 17:31:42 -0500

    post and delete

--- index.js
+++ index.js
@@ -42,3 +42,43 @@ server.get("/api/users/:id", function(req, res) {
 				.json({ error: "The user information could not be retrieved." });
 		});
 });
+
+server.post("/api/users", (req, res) => {
+	const userInfo = req.body;
+	console.log(userInfo);
+
+	if (userInfo.name && userInfo.bio) {
+		users
+			.insert(userInfo)
+			.then(user => {
+				res.status(201).json(user);
+			})
+			.catch(error => {
+				res.status(500).json({
+					error: "There was an error while saving the user to the database"
+				});
+			});
+	} else {
+		res
+			.status(400)
+			.json({ errorMessage: "Please provide name and bio for the user." });
+	}
+});
+
+server.delete("/api/users/:id", (req, res) => {
+	const { id } = req.params;
+	users
+		.remove(id)
+		.then(deleted => {
+			if (deleted) {
+				res.status(204).end();
+			} else {
+				res
+					.status(404)
+					.json({ message: "The user with the specified ID does not exist." });
+			}
+		})
+		.catch(error => {
+			res.status(500).json({ error: "The user could not be removed" });
+		});
+});
